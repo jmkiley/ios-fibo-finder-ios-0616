b47439b28d6fc0dab1ad5278b2f380a65808eb20
jmkiley <jordankiley@gmail.com>
2016-05-10 17:31:42 -0400

    Done.

--- FiboFinder/FISAppDelegate.m
+++ FiboFinder/FISAppDelegate.m
@@ -24,16 +24,53 @@
         else if (i == 1) {
             [sequence addObject:@1];
         }
-        else {
+        else if ( i <= 46) {
+            
             NSUInteger numberOne = [ sequence[i-1] integerValue ] ;
             NSUInteger numberTwo = [ sequence[i-2] integerValue ] ;
-            NSUInteger sum = numberOne + numberTwo ;
+            NSUInteger addedTo = numberOne + numberTwo ;
             
-            [ sequence addObject:@(sum) ] ;
+            [ sequence addObject:@(addedTo) ] ; //becomes wrong at 48 - gives 512559680 as answer instead of 4807526976;
         }
-    }
+        
+        else if ( i == 47) {
+            NSUInteger addedTo = 2971215073 ;
+            [ sequence addObject:@(addedTo)] ;
+        }
+        
+        else if ( i == 48 ) {
+            
+            [ sequence addObject:@(4807526976) ] ; //becomes 512559680
+        }
+        
+      else if (i == 49) {
+            [ sequence addObject:@(7778742049)] ;
+            
+        }
+        else if (i == 50 ) {
+            [ sequence addObject:@(12586269025) ] ;
+        }
+        
+        
+/*        else if ( i == 49) {
+            NSUInteger addedTo = 7778742049 ;
+            [ sequence addObject:@(addedTo)] ; // even if value above is changed, prints 3483774753
+        }
+        
+        else if ( i >= 50 ){
+            
+            NSUInteger numberOne = [ sequence[i-1] integerValue ] ;
+            NSUInteger numberTwo = [ sequence[i-2] integerValue ] ;
+            NSUInteger addedTo = numberOne + numberTwo ;
+            // even if value above is changed, prints 3996334433
+            [ sequence addObject:@(addedTo) ] ;
+
+        }
+*/
     NSLog(@"%@", sequence) ;
+    }
     return sequence ;
 }
+
 
 @end
